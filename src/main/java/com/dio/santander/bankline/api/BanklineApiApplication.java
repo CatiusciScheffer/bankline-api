53b60a16e40721def1963b9e62fe546f41c715a2
Catiusci Scheffer <cpcscheffer@outlook.com>
2022-05-07 16:57:38 -0300

    swagger configurado erro versao corrigido

--- src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -2,8 +2,7 @@ package com.dio.santander.bankline.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class BanklineApiApplication {
